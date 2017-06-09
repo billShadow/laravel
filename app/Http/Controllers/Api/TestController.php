5e992f7e0cc3e7ebaa75ea5770d7ad6da9799bad
xiaobin <xiaobin@e7124.com>
2017-06-09 20:14:17 +0800

    h5 show image

--- app/Http/Controllers/Api/TestController.php
+++ app/Http/Controllers/Api/TestController.php
@@ -174,4 +174,6 @@ class TestController extends Controller
 
 
 
+
+
 }
--- resources/views/admin/power/addrole.blade.php
+++ resources/views/admin/power/addrole.blade.php
@@ -24,6 +24,15 @@
                             <input type="text" name="role_desc" placeholder="请填写角色描述" value="" class="layui-input" >
                         </div>
                     </div>
+                    <div class="layui-form-item">
+                        <label class="layui-form-label">banner图：</label>
+                        <div class="layui-box layui-upload-button">
+                            <input type="file" name="images" class="layui-upload-file" id="banner_one">
+                            <span class="layui-upload-icon"><i class="layui-icon"></i>上传图片</span>
+                        </div>
+                        <span style="color: red">建议：图片文件大小在1M以内，以保证用户浏览时的不会出现加载过慢的情况</span>
+                        <div id="content_banner_one" style="margin-left: 15%"></div>
+                    </div>
                 </div>
             </fieldset>
             <div class="layui-form-item text-center" >
@@ -33,6 +42,25 @@
     </div>
     <script>
         $(function () {
+            $('#banner_one').change(function(){
+                var inputElement = document.getElementById('banner_one');
+                var fileList = this.files;
+                var reader = new FileReader();
+                reader.readAsDataURL(fileList[0]);
+                console.log(fileList[0].type);
+                reader.onload = function(e) {
+                    var image = new Image();
+                    image.src = e.target.result;
+                    image.onload=function(){
+                        console.log(image.width);
+                        console.log(image.height);
+                        console.log();
+                        var bili = image.width/image.height;
+                        var www = 120/bili;
+                        $('#content_banner_one').html("<img style='float:left;margin:10px 0px 10px 180px;' src='"+image.src+"' width='120px' height='"+www+"px'/>");
+                    }
+                };
+            });
 
             $('#form_data').submit(function(){
                 var $role_name = $('input[name=role_name]').val();
